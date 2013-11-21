432a962409ee8b5f1cb59abafadd08210632620a
Jace Browning <jacebrowning@gmail.com>
2013-11-21 00:12:30 -0500

    0.0.1-alpha.2 - added classifiers

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if not os.path.exists(README):
 
 setuptools.setup(
     name=__project__,
-    version='0.0.1-alpha.1',
+    version='0.0.1-alpha.2',
 
     description="Music sharing using Dropbox.",
     url='http://pypi.python.org/pypi/DropTheBeat',
@@ -30,6 +30,21 @@ setuptools.setup(
 
     long_description=open(README).read(),
     license='LGPL',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Environment :: MacOS X',
+        'Environment :: Win32 (MS Windows)',
+        'Environment :: X11 Applications',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',  # pylint: disable=C0301
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Communications :: File Sharing',
+        'Topic :: Multimedia :: Sound/Audio',
+    ],
 
-    install_requires=["wheel", "PyYAML==3.10", "sqlalchemy==0.8.3"],
+    install_requires=["PyYAML==3.10", "sqlalchemy==0.8.3"],
 )
