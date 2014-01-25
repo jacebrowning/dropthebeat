86d16966ddf29b4e4b9e2997781c0d211ec06bc7
Jace Browning <jacebrowning@gmail.com>
2014-01-25 18:01:18 -0500

    0.0.6

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if not os.path.exists(README):
 
 setuptools.setup(
     name=__project__,
-    version='0.0.5',
+    version='0.0.6',
 
     description="Music sharing using Dropbox.",
     url='http://github.com/jacebrowning/dropthebeat',
@@ -31,7 +31,7 @@ setuptools.setup(
     long_description=open(README).read(),
     license='LGPL',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'Environment :: MacOS X',
         'Environment :: Win32 (MS Windows)',
@@ -40,7 +40,6 @@ setuptools.setup(
         'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',  # pylint: disable=C0301
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Topic :: Communications :: File Sharing',
         'Topic :: Multimedia :: Sound/Audio',
