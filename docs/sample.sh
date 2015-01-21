931760f08f64546127df14917ad35eca3b0a0b9e
Jace Browning <jacebrowning@gmail.com>
2015-01-20 23:03:20 -0500

    Explain that spaces are allowed in usernames
    There’s no reason they should be discouraged and it looks better.

--- docs/sample.sh
+++ docs/sample.sh
@@ -7,8 +7,8 @@ set -x
 SCREENSHOT="assets/screenshot.png"
 
 ROOT="/Users/Browning/Dropbox/DropTheBeat"
-USER1="JohnDoe"
-USER2="JaneDoe"
+USER1="John Doe"
+USER2="Jane Doe"
 USER3="Me"
 FILE1="/tmp/New Mashup 2015.mp3"
 FILE2="/tmp/My Demo Tracks.zip"
@@ -18,16 +18,19 @@ touch "$FILE1"
 touch "$FILE2"
 touch "$FILE3"
 
-dtb --root $ROOT --new $USER1
-dtb --root $ROOT --new $USER2
-dtb --root $ROOT --new $USER3
+dtb --root $ROOT --new "$USER1"
+dtb --root $ROOT --new "$USER2"
+dtb --root $ROOT --new "$USER3"
 
-dtb --root $ROOT --test $USER3 --share "$FILE1"
-dtb --root $ROOT --test $USER3 --share "$FILE2" --user $USER2
-dtb --root $ROOT --test $USER1 --share "$FILE3"
+dtb --root $ROOT --test "$USER3" --share "$FILE1"
+dtb --root $ROOT --test "$USER3" --share "$FILE2" --user "$USER2"
+dtb --root $ROOT --test "$USER1" --share "$FILE3"
 
-echo "save a screenshot to: $SCREENSHOT"
-dtb --root $ROOT --test $USER3 --gui
+echo
+echo "save a 0.5x screenshot to: $SCREENSHOT"
+echo
+
+dtb --root $ROOT --test "$USER3" --gui
 
 rm -r $ROOT
 
