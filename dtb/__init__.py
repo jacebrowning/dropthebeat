72020edb94f976e896d965b85530ac8c9546f160
Jace Browning <jacebrowning@gmail.com>
2015-03-21 10:26:34 -0400

    Fix imports for PEP8

--- dtb/__init__.py
+++ dtb/__init__.py
@@ -1,5 +1,7 @@
 """Package for DropTheBeat."""
 
+import sys
+
 __project__ = 'DropTheBeat'
 __version__ = '0.2-dev'
 
@@ -9,6 +11,5 @@ VERSION = __project__ + '-' + __version__
 
 PYTHON_VERSION = 3, 3
 
-import sys
 if not sys.version_info >= PYTHON_VERSION:  # pragma: no cover (manual test)
     exit("Python {}.{}+ is required.".format(*PYTHON_VERSION))
